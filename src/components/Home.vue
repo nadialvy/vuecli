ef4da2e4ed7735e70711f68251de49aabb475cc3
nadialvy <nadia_lovely_29rpl@student.smktelkom-mlg.sch.id>
2022-04-06 07:33:03 +0700

    Adding carousel on home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,30 @@
 <template>
     <div class="container">
-        <div class="card">
-            <div class="card-body">
-                <h2>WELCOME!</h2>
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active" style="filter: invert(100%);"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1" style="filter: invert(100%);"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2" style="filter: invert(100%);"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="d-block w-75" src="./../assets/images/suho.png" alt="First slide" style="object-fit: cover; margin: auto">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-75" src="./../assets/images/exo.png" alt="Second slide" style="object-fit: cover; margin: auto">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-75" src="./../assets/images/nct.png" alt="Third slide" style="object-fit: cover; margin: auto">
+                </div>
             </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev" style="filter: invert(100%);">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next" style="filter: invert(100%);">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
     </div>
 </template>
